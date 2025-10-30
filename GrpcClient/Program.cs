e42883085ceab27addb448eaedbd9e6355a262a8
thanhphuongne <lephuong14052004@gmail.com>
2025-10-30 11:01:34 +0700

    first

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,19 @@
+﻿using Grpc.Net.Client;
+using GrpcServer;
+
+using var channel = GrpcChannel.ForAddress("http://localhost:5220");
+var client = new Greeter.GreeterClient(channel);
+
+Console.Write("Enter your name: ");
+var name = Console.ReadLine();
+
+if (string.IsNullOrEmpty(name))
+{
+    name = "World";
+}
+
+var reply = await client.SayHelloAsync(new HelloRequest { Name = name });
+Console.WriteLine($"Greeting: {reply.Message}");
+
+Console.WriteLine("Press any key to exit...");
+Console.ReadLine();
